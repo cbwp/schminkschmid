a27918e18ce59d2511956526810ffbc4c647cc8d
cbwp <cb@cbwp.ch>
2017-09-13 18:39:12 +0200

    online db

--- application/config/database.php
+++ application/config/database.php
@@ -6,9 +6,9 @@ return [
         'concrete' => [
             'driver' => 'c5_pdo_mysql',
             'server' => 'localhost',
-            'database' => 'schminkschmid',
-            'username' => 'root',
-            'password' => '',
+            'database' => 'schmink_c5',
+            'username' => 'schmink_c5',
+            'password' => '6hlV&y51',
             'charset' => 'utf8',
         ],
     ],
